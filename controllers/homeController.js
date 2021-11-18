95cb2b4b156c8ee7314a501b1f55849c9a46d4af
risangdevs <risangdevs@gmail.com>
2021-11-18 13:16:20 +0700

    added genre movie filter

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,11 +3,20 @@ const { Movie } = require("../models");
 const { formatDate } = require("../helpers/dateFormatter");
 const formatMonetary = require("../helpers/monetaryFormatter")
 
+//filter by genre
+// - Drama
+// - Action
+// - Sci-fi
+// - Biography
+// - Adventure
+// - Comedy
+
 class HomeController {
     static readMovie(req, res) {
+        // console.log(req.query.genre);
         const { genre, search } = req.query;
         const filterQuery = {}
-        if (genre) filterQuery.genre = { [Op.not]: genre }
+        if (genre) filterQuery.genre = { [Op.eq]: genre }
         if (search) filterQuery.title = { [Op.iLike]: '%' + search + '%' }
 
         console.log(filterQuery);
